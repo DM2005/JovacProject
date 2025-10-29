25241cac49138520ef46d86a9daa353adf1d9c23
DM2005 <mdevansh0109@gmail.com>
2025-10-30 00:08:29 +0530

    Delete server directory( wrong commit)

--- server/server.js
+++
@@ -1,9 +0,0 @@
-// server.js - Main server file
-const express = require('express');
-require('dotenv').config();
-const app = require('./app');
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
